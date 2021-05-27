a20cc27819ffba6a43714f0708d99d05a5ddddd1
israelpablo <israel.lopez17@tectijuana.edu.mx>
2021-05-27 01:46:32 -0700

    Actualizaciones

--- VentaTikect/app/routes.js
+++ VentaTikect/app/routes.js
@@ -9,39 +9,42 @@ module.exports=router;
 
 admin=`<header>
 <nav class="navbar navbar-inverse">
-  <a href="/Sesion" class="navbar-brand">Inicio de sesion</a>
+  <a href="/UsuarioActivo" class="navbar-brand">NombreUser</a>
+  <a href="/Cerrar" class="navbar-brand">Cerrar Sesion</a>
   <ul class="nav navbar-nav">
-    <li><a href="/">Home</a></li>
-    <li><a href="/Venta">Comprar boleta</a></li>
-    <li><a href="/ConsultaH">Historial de  boleto</a></li>
-    <li><a href="/Hitoria">Historial de movimientos</a></li>
-    <li><a href="/Alto">Alta usuario </a></li>
-    <li><a href="/about">About</a></li>
-    <li><a href="/contact">Contact</a></li>
+    <li><a href="/ID">Home</a></li>
+    <li><a href="/Venta/ID">Comprar boleta</a></li>
+    <li><a href="/ConsultaH/ID">Historial de  boleto</a></li>
+    <li><a href="/Hitoria/ID">Historial de movimientos</a></li>
+    <li><a href="/Alto/ID">Alta usuario </a></li>
+    <li><a href="/about/ID">About</a></li>
+    <li><a href="/contact/ID">Contact</a></li>
   </ul>
 </nav>
 </header>`;
 comun=`<header>
 <nav class="navbar navbar-inverse">
-  <a href="/Sesion" class="navbar-brand">Inicio de sesion</a>
+<a href="/UsuarioActivo" class="navbar-brand">NombreUser</a>
+<a href="/Cerrar" class="navbar-brand">Cerrar Sesion</a>
   <ul class="nav navbar-nav">
-    <li><a href="/">Home</a></li>
-    <li><a href="/Venta">Comprar boleta</a></li>
-    <li><a href="/ConsultaH">Historial de  boleto</a></li>
-    <li><a href="/about">About</a></li>
-    <li><a href="/contact">Contact</a></li>
+    <li><a href="/ID">Home</a></li>
+    <li><a href="/Venta/ID">Comprar boleta</a></li>
+    <li><a href="/ConsultaH/ID">Historial de  boleto</a></li>
+    <li><a href="/about/ID">About</a></li>
+    <li><a href="/contact/ID">Contact</a></li>
   </ul>
 </nav>
 </header>`;
 chofer=`<header>
 <nav class="navbar navbar-inverse">
-  <a href="/Sesion" class="navbar-brand">Inicio de sesion</a>
+<a href="/UsuarioActivo" class="navbar-brand">NombreUser</a>
+<a href="/Cerrar" class="navbar-brand">Cerrar Sesion</a>
   <ul class="nav navbar-nav">
-    <li><a href="/">Home</a></li>
-    <li><a href="/Captura">Capturar boleto</a></li>
-    <li><a href="/HistorialC">Total Boletos</a></li>
-    <li><a href="/about">About</a></li>
-    <li><a href="/contact">Contact</a></li>
+    <li><a href="/ID">Home</a></li>
+    <li><a href="/Captura/ID">Capturar boleto</a></li>
+    <li><a href="/HistorialC/ID">Total Boletos</a></li>
+    <li><a href="/about/ID">About</a></li>
+    <li><a href="/contact/ID">Contact</a></li>
   </ul>
 </nav>
 </header>`;
@@ -56,12 +59,149 @@ defaults=`<header>
   </ul>
 </nav>
 </header>`;
+var  activeuser=[];
+var ids;
+//Sesion default
 
-
-
-
-router.get('/', function(req, res) {
+  router.get('/', function(req, res) {
     res.render('pages/home',{def:defaults});
+  });
+  router.get('/Sesion', function(req, res) {
+    res.render('pages/Sesion',{def:defaults});
+  });
+  router.get('/about', function(req, res) {
+    res.render('pages/about',{def:defaults});
+  });
+  router.get('/contact', function(req, res) {
+    res.render('pages/contact',{def:defaults});
+  });
+//sesion todo los demas
+var tipouser;
+bandera=false;
+var iduse,nombreuse;
+  const dbAcceso = require('../app/Procedures/Accesos');
+  router.post('/Sesion', function(req, res) {
+   
+var menu;
+
+    pass=req.body.Pass_TXT;
+    users=req.body.User_txt;
+    dbAcceso.Accesos(users,pass).then(result=>{
+    act(result);
+console.log(tipouser)
+banderas= false;
+     counter=0;
+   revisar=activeuser;
+revisar.forEach(IndexConexion); 
+      if(banderas==true)
+{      activeuser.splice(counter,1);}
+    switch(tipouser)
+    {
+      case 'Comun':
+        menu=comun;
+        menu=menu.replace('NombreUser',nombreuse);
+        menu=menu.replace('ID',iduse);
+        menu=menu.replace('ID',iduse);
+        menu=menu.replace('ID',iduse);
+        menu=menu.replace('ID',iduse);
+        menu=menu.replace('ID',iduse);
+        menu=menu.replace('ID',iduse);
+        menu=menu.replace('ID',iduse);
+        menu=menu.replace('ID',iduse);
+        res.render('pages/home',{def:menu});
+
+    break;
+    case 'Admin':
+      menu=admin;
+      menu=menu.replace('NombreUser',nombreuse);
+      menu=menu.replace('ID',iduse);
+      menu=menu.replace('ID',iduse);
+      menu=menu.replace('ID',iduse);
+      menu=menu.replace('ID',iduse);
+      menu=menu.replace('ID',iduse);
+      menu=menu.replace('ID',iduse);
+      menu=menu.replace('ID',iduse);
+      menu=menu.replace('ID',iduse);
+      res.render('pages/home',{def:admin});
+      break;
+      case 'Chofer':
+        menu=chofer;
+        menu=menu.replace('NombreUser',nombreuse);
+        menu=menu.replace('ID',iduse);
+        menu=menu.replace('ID',iduse);
+        menu=menu.replace('ID',iduse);
+        menu=menu.replace('ID',iduse);
+        menu=menu.replace('ID',iduse);
+        menu=menu.replace('ID',iduse);
+        menu=menu.replace('ID',iduse);
+        menu=menu.replace('ID',iduse);
+        res.render('pages/home',{def:chofer});
+        break;
+        case 'Default':
+          res.render('pages/home',{def:defaults});
+          break;
+   
+    
+    }
+      })
+    
+  });
+
+function IndexConexion(element,index,array)
+{
+  if (element.IdUsuario==ids)
+  {
+    banderas=true;
+     return counter;
+  } 
+  else
+  {
+   counter=counter+1
+  }      
+}
+  function act(recibir)
+  {
+  activo=recibir;
+  activo[0].forEach(logArrayElements2);
+  }
+  function logArrayElements2(element, index, array) {
+    const contact={
+      ID_USER: element.IdUsuario,
+      USERNAME: element.Usuario,
+      PASS: element.Pass,
+      FNAME: element.Nombre,
+    LNAME: element.ApellidoP,
+    LNAME2:element.ApellidoM,
+      ROLE:element.NombreAcceso
+    };
+    activeuser.push(contact);
+       tipouser= element.NombreAcceso;
+       iduse=element.IdUsuario;
+       nombreuse=element.Usuario;
+   }
+   router.get('/Cerrar', function(req, res) {
+    banderas= false;
+    counter=0;
+  revisar=activeuser;
+revisar.forEach(IndexConexion); 
+     if(banderas==true)
+{      activeuser.splice(counter,1);}
+    res.render('pages/home',{def:defaults});
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+  router.get('/:hola', function(req, res) {
+    res.render('pages/home',{def:comun});
   });
   router.get('/Sesion', function(req, res) {
     res.render('pages/Sesion');
@@ -81,40 +221,4 @@ router.get('/', function(req, res) {
   router.get('/usernew', function(req, res) {
     res.render('pages/usernew');
   });
-  var tipouser
-  const dbAcceso = require('../app/Procedures/Accesos');
-  router.post('/Sesion', function(req, res) {
-   
-    pass=req.body.Pass_TXT;
-    users=req.body.User_txt;
-    dbAcceso.Accesos(users,pass).then(result=>{
-    act(result);
-console.log(tipouser)
-    switch(tipouser)
-    {
-      case 'Comun':
-        res.render('pages/home',{def:comun});
-    break;
-    case 'Admin':
-      res.render('pages/home',{def:admin});
-      break;
-      case 'Chofer':
-        res.render('pages/home',{def:chofer});
-        break;
-        case 'Default':
-          res.render('pages/home',{def:defaults});
-          break;
-   
-    
-    }
-      })
-    
-  });
-  function act(recibir)
-  {
-  activo=recibir;
-  activo[0].forEach(logArrayElements2);
-  }
-  function logArrayElements2(element, index, array) {
-       tipouser= element.NombreAcceso;
-   }
+  
